3859efba4dbcb1728094725d2f19a9f386c8c318
marteom <marteom@gmail.com>
2024-03-11 17:13:01 +0300

    feat: add album.service and split code

--- src/album/album.controller.ts
+++ src/album/album.controller.ts
@@ -3,157 +3,46 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   HttpStatus,
   Param,
   Post,
   Put,
-  Res,
 } from '@nestjs/common';
 
 import { AlbumModel } from './album.model';
-import { albumsData } from './data/album.data.js';
-import { Response } from 'express';
-import { isIdValid } from '../utils/common-utils';
-import { getAlbum } from './utils/helper.js';
-import {
-  delAlbumFromFavorites,
-  nulledAlbumForTrack,
-} from '../utils/common-utils';
-import { tracksData } from 'src/track/data/track.data';
+import { AlbumService } from './album.service';
 
 @Controller('album')
 export class AlbumController {
+  constructor(private readonly albumService: AlbumService) {}
+
   @Get()
   async getAllAlbums() {
-    return albumsData;
+    return this.albumService.getAllAlbums();
   }
 
   @Get(':id')
-  async getAlbumById(@Param('id') id: string, @Res() response: Response) {
-    if (!(await isIdValid(id))) {
-      return response
-        .status(HttpStatus.BAD_REQUEST)
-        .send('id parameter is invalid (not uuid)');
-    }
-
-    const album: AlbumModel = albumsData.find(
-      (element: AlbumModel) => element.id === id,
-    );
-
-    if (album) {
-      return response.status(HttpStatus.OK).send(album);
-    }
-
-    return response.status(HttpStatus.NOT_FOUND).send('Album not found');
+  async getAlbumById(@Param('id') id: string) {
+    return this.albumService.getAlbumById(id);
   }
 
   @Post()
-  async CreateAlbumDto(
-    @Body() dto: Omit<AlbumModel, 'id'>,
-    @Res() response: Response,
-  ) {
-    if (!dto.name) {
-      return response
-        .status(HttpStatus.BAD_REQUEST)
-        .send('required parameter "name" is missing');
-    }
-
-    if (!dto.name) {
-      return response
-        .status(HttpStatus.BAD_REQUEST)
-        .send('required parameter "name" is missing');
-    }
-
-    if (!dto.year || typeof dto.year !== 'number') {
-      return response
-        .status(HttpStatus.BAD_REQUEST)
-        .send('required parameter "year" is missing or have incorrect type');
-    }
-
-    if (
-      dto.artistId === undefined ||
-      !(dto.artistId === null ? true : await isIdValid(dto.artistId))
-    ) {
-      return response
-        .status(HttpStatus.BAD_REQUEST)
-        .send('required parameter "artistId" is missing or invalid (not uuid)');
-    }
-
-    const newAlbum = await getAlbum(dto.name, dto.year, dto.artistId);
-    albumsData.push(newAlbum);
-    return response.status(HttpStatus.CREATED).send(newAlbum);
+  async CreateAlbum(@Body() dto: Omit<AlbumModel, 'id'>) {
+    return this.albumService.CreateAlbum(dto);
   }
 
   @Put(':id')
   async updateAlbumInfo(
     @Param('id') id: string,
     @Body() dto: Omit<AlbumModel, 'id'>,
-    @Res() response: Response,
   ) {
-    if (!(await isIdValid(id))) {
-      return response
-        .status(HttpStatus.BAD_REQUEST)
-        .send('id parameter is invalid (not uuid)');
-    }
-
-    if (!dto.name) {
-      return response
-        .status(HttpStatus.BAD_REQUEST)
-        .send('required parameter "name" is missing');
-    }
-
-    if (!dto.year || typeof dto.year !== 'number') {
-      return response
-        .status(HttpStatus.BAD_REQUEST)
-        .send('required parameter "year" is missing or have incorrect type');
-    }
-
-    if (
-      dto.artistId === undefined ||
-      !(dto.artistId === null ? true : await isIdValid(dto.artistId))
-    ) {
-      return response
-        .status(HttpStatus.BAD_REQUEST)
-        .send('required parameter "artistId" is missing or invalid (not uuid)');
-    }
-
-    const putedAlbumIndex = albumsData.findIndex(
-      (element: AlbumModel) => element.id === id,
-    );
-
-    if (putedAlbumIndex === -1) {
-      return response.status(HttpStatus.NOT_FOUND).send('Album not found');
-    }
-
-    albumsData[putedAlbumIndex].name = dto.name;
-    albumsData[putedAlbumIndex].year = dto.year;
-    albumsData[putedAlbumIndex].artistId = dto.artistId;
-
-    return response.status(HttpStatus.OK).send(albumsData[putedAlbumIndex]);
+    return this.albumService.updateAlbumInfo(id, dto);
   }
 
   @Delete(':id')
-  async deleteAlbumById(@Param('id') id: string, @Res() response: Response) {
-    if (!(await isIdValid(id))) {
-      return response
-        .status(HttpStatus.BAD_REQUEST)
-        .send('id parameter is invalid (not uuid)');
-    }
-
-    const deletedAlbumIndex = albumsData.findIndex(
-      (element: AlbumModel) => element.id === id,
-    );
-
-    if (deletedAlbumIndex === -1) {
-      return response.status(HttpStatus.NOT_FOUND).send('Album not found');
-    }
-
-    albumsData.splice(deletedAlbumIndex, 1);
-    await delAlbumFromFavorites(id);
-    await nulledAlbumForTrack(tracksData, id);
-
-    return response
-      .status(HttpStatus.NO_CONTENT)
-      .send('Album successfully deleted');
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async deleteAlbumById(@Param('id') id: string) {
+    return this.albumService.deleteAlbumById(id);
   }
 }
---
+++ src/album/album.service.ts
@@ -0,0 +1,135 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { albumsData } from './data/album.data';
+import {
+  delAlbumFromFavorites,
+  isIdValid,
+  nulledAlbumForTrack,
+} from 'src/utils/common-utils';
+import { AlbumModel } from './album.model';
+import { getAlbum } from './utils/helper';
+import { tracksData } from 'src/track/data/track.data';
+
+@Injectable()
+export class AlbumService {
+  async getAllAlbums() {
+    return albumsData;
+  }
+
+  async getAlbumById(id: string) {
+    if (!(await isIdValid(id))) {
+      throw new HttpException(
+        'id parameter is invalid (not uuid)',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const album: AlbumModel = albumsData.find(
+      (element: AlbumModel) => element.id === id,
+    );
+
+    if (album) {
+      return album;
+    }
+
+    throw new HttpException('Album not found', HttpStatus.NOT_FOUND);
+  }
+
+  async CreateAlbum(dto: Omit<AlbumModel, 'id'>) {
+    if (!dto.name) {
+      throw new HttpException(
+        'required parameter "name" is missing',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    if (!dto.year || typeof dto.year !== 'number') {
+      throw new HttpException(
+        'required parameter "year" is missing or have incorrect type',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    if (
+      dto.artistId === undefined ||
+      !(dto.artistId === null ? true : await isIdValid(dto.artistId))
+    ) {
+      throw new HttpException(
+        'required parameter "artistId" is missing or invalid (not uuid)',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const newAlbum = await getAlbum(dto.name, dto.year, dto.artistId);
+    albumsData.push(newAlbum);
+    return newAlbum;
+  }
+
+  async updateAlbumInfo(id: string, dto: Omit<AlbumModel, 'id'>) {
+    if (!(await isIdValid(id))) {
+      throw new HttpException(
+        'id parameter is invalid (not uuid)',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    if (!dto.name) {
+      throw new HttpException(
+        'required parameter "name" is missing',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    if (!dto.year || typeof dto.year !== 'number') {
+      throw new HttpException(
+        'required parameter "year" is missing or have incorrect type',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    if (
+      dto.artistId === undefined ||
+      !(dto.artistId === null ? true : await isIdValid(dto.artistId))
+    ) {
+      throw new HttpException(
+        'required parameter "artistId" is missing or invalid (not uuid)',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const putedAlbumIndex = albumsData.findIndex(
+      (element: AlbumModel) => element.id === id,
+    );
+
+    if (putedAlbumIndex === -1) {
+      throw new HttpException('Album not found', HttpStatus.NOT_FOUND);
+    }
+
+    albumsData[putedAlbumIndex].name = dto.name;
+    albumsData[putedAlbumIndex].year = dto.year;
+    albumsData[putedAlbumIndex].artistId = dto.artistId;
+
+    return albumsData[putedAlbumIndex];
+  }
+
+  async deleteAlbumById(id: string) {
+    if (!(await isIdValid(id))) {
+      throw new HttpException(
+        'id parameter is invalid (not uuid)',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const deletedAlbumIndex = albumsData.findIndex(
+      (element: AlbumModel) => element.id === id,
+    );
+
+    if (deletedAlbumIndex === -1) {
+      throw new HttpException('Album not found', HttpStatus.NOT_FOUND);
+    }
+
+    await delAlbumFromFavorites(id);
+    await nulledAlbumForTrack(tracksData, id);
+
+    return albumsData.splice(deletedAlbumIndex, 1);
+  }
+}
